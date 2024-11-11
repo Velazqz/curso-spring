47bb20e254736766df5d5b2df39096a195d8a7c1
ValerieVelazquez <vg.velazquez@ebc.edu.mx>
2024-11-11 12:30:19 -0600

    config add project for exercise 1

---
+++ multiplicacion/src/main/java/microservices/book/multiplicacion/MultiplicacionApplication.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplicacionApplication.class, args);
+	}
+
+}
---
+++ multiplicacion/src/test/java/microservices/book/multiplicacion/MultiplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
