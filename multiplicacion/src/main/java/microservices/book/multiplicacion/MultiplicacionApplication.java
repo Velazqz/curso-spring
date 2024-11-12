be589374a7d061c867abb015e41a35a4f4dd1638
ValerieVelazquez <vg.velazquez@ebc.edu.mx>
2024-11-12 11:08:51 -0600

    config add dependencies in pom file

--- multiplicacion/src/main/java/microservices/book/multiplicacion/MultiplicacionApplication.java
+++ multiplicacion/src/main/java/microservices/book/multiplicacion/MultiplicacionApplication.java
@@ -1,9 +1,5 @@
 package microservices.book.multiplicacion;
 
-<<<<<<< Updated upstream
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-=======
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
@@ -11,23 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import microservices.book.multiplicacion.service.GreetingMessageService;
->>>>>>> Stashed changes
 
 @SpringBootApplication
 public class MultiplicacionApplication {
 
 	public static void main(String[] args) {
-<<<<<<< Updated upstream
-		SpringApplication.run(MultiplicacionApplication.class, args);
-	}
-
-=======
 		ApplicationContext ctx = SpringApplication.run(MultiplicacionApplication.class,args);
 		String[] beanNames = ctx.getBeanDefinitionNames();
 		Arrays.sort(beanNames);
 		for (String beanName : beanNames) {
-		 System.out.println(beanName); 
+		System.out.println(beanName);
 		}
-	  } 
->>>>>>> Stashed changes
+	}
 }
