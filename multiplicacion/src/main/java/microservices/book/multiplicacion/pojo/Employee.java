3065db878d5ac374d04f9a85498a06f426957b18
ValerieVelazquez <vg.velazquez@ebc.edu.mx>
2024-11-13 08:32:19 -0600

    coding add employee pojo

---
+++ multiplicacion/src/main/java/microservices/book/multiplicacion/pojo/Employee.java
@@ -0,0 +1,16 @@
+package microservices.book.multiplicacion.pojo;
+
+public class Employee {
+
+	private int employeeId;
+
+	public String toString() {
+		 return "Employee [employeeId: " + employeeId + "]";
+	}
+	
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+
+}
