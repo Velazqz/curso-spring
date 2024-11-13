dec073f766aa9c3e1b0b5ab6505fc802eab5a961
ValerieVelazquez <vg.velazquez@ebc.edu.mx>
2024-11-13 08:07:49 -0600

    config application class using dependecy injections constructor for mores

--- multiplicacion/src/main/java/microservices/book/multiplicacion/service/EmployeeService.java
+++ multiplicacion/src/main/java/microservices/book/multiplicacion/service/EmployeeService.java
@@ -1,14 +1,28 @@
 package microservices.book.multiplicacion.service;
 
 public class EmployeeService {
-    private String greeting;
+	
+    private String employeeName;
+    private int employeeAge;
+    private String employeeId;
 
-    public EmployeeService(String greeting){
-		this.greeting = greeting;
+    public EmployeeService(String employeeName,int employeeAge,String employeeId){
+        System.out.println("Primer constructor");
+        this.employeeName=employeeName;
+        this.employeeAge=employeeAge;
+        this.employeeId=employeeId;
+   }
+
+    public EmployeeService(String employeeName,String employeeId,int employeeAge){
+        System.out.println("Segundo constructor");
+        this.employeeName=employeeName;
+        this.employeeAge=employeeAge;
+        this.employeeId=employeeId;
     }
 
     @Override
     public String toString() {
-        return this.greeting;
+        return "Employee name: "+employeeName+" Age: "+employeeAge+" EmployeeId: "+employeeId;
     }
- }
+
+}
