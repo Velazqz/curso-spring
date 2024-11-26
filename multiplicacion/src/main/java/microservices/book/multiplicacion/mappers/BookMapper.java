999261c3e1fb11ab6d6c382b4df84ec5a9f0498f
ValerieVelazquez <vg.velazquez@ebc.edu.mx>
2024-11-26 11:56:55 -0600

    coding add mappers

---
+++ multiplicacion/src/main/java/microservices/book/multiplicacion/mappers/BookMapper.java
@@ -0,0 +1,25 @@
+package microservices.book.multiplicacion.mappers;
+
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import microservices.book.multiplicacion.data.BookDto;
+import microservices.book.multiplicacion.entity.Book;
+
+@Mapper
+public interface BookMapper {
+	
+	BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+	BookDto bookToBookDto(Book book);
+	
+	Book bookDtoToBook(BookDto bookDto);
+	
+	List<BookDto> bookListToBookDtoList(List<Book> books);
+	
+	List<Book> bookDtoListToBookList(List<BookDto> books);
+
+}
