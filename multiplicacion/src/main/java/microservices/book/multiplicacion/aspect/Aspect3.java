1f0ccdd335fb20d73a0638ce39f7d7a382492c94
ValerieVelazquez <vg.velazquez@ebc.edu.mx>
2024-11-28 10:51:40 -0600

    add aspect advice after running

---
+++ multiplicacion/src/main/java/microservices/book/multiplicacion/aspect/Aspect3.java
@@ -0,0 +1,30 @@
+package microservices.book.multiplicacion.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class Aspect3 {
+	
+	// advice - afterReturnnig 
+	// todos los metodos de las clases en el paquete entities
+	// desplegar clase y nombre de metodo y el valor de retorno del metodo
+	
+	
+    @AfterReturning(
+        pointcut = "execution(* microservices.book.multiplicacion.entities..*(..))",
+        returning = "returnValue"
+    )
+    public void logReturnValue(JoinPoint joinPoint, Object returnValue) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println(String.format(
+            "Method executed: %s.%s, Return Value: %s",
+            className, methodName, returnValue
+        ));
+    }
+
+}
