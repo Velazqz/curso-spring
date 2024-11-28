bf71608e39f1411d1e9439871e525e8e691eb2b2
ValerieVelazquez <vg.velazquez@ebc.edu.mx>
2024-11-28 10:20:57 -0600

    add method set in main

--- multiplicacion/src/main/java/microservices/book/multiplicacion/MultiplicacionApplication.java
+++ multiplicacion/src/main/java/microservices/book/multiplicacion/MultiplicacionApplication.java
@@ -4,16 +4,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import microservices.book.multiplicacion.entities.Game;
+import microservices.book.multiplicacion.entities.Team;
 
 //@SpringBootApplication
 public class MultiplicacionApplication {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-		Game game1 = context.getBean("game",Game.class);
-		System.out.println("****|Game|****");
-		System.out.println(game1.playGame());
-
+	ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		Game game1 = context.getBean("game", Game.class);
+		Team favoriteTeam = context.getBean("redSox", Team.class);
+		game1.setAwayTeam(favoriteTeam);
+		System.out.println("****** | Game | ****** \n " + game1.playGame());
 	}
 
 }
